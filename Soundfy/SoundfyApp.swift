1ad082b0f812724053c9e14bada1923daf1a4b89
Pedro Pessuto <pedro.pessuto@outlook.com>
2023-09-22 14:57:51 -0300

    Initial Commit

---
+++ Soundfy/SoundfyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundfyApp.swift
+//  Soundfy
+//
+//  Created by Pedro Pessuto on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SoundfyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
